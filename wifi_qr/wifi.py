23a67830174852409209a0cd4ef0a6bd7b5658c5
Apollonrayan <78968453+Apollonrayan@users.noreply.github.com>
2023-04-10 13:31:19 +0200

    Delete wifi_qr directory

--- wifi_qr/wifi.py
+++
@@ -1,9 +0,0 @@
-import segno
-from segno import helpers
-import os
-
-qrcode = helpers.make_wifi(ssid='Campus Condorcet Colloques', password='Campus93!', security='WPA')
-qrcode.designator
-'3-M'
-qrcode.save('wifi-access.png', scale=10)
-
